c35fb605f1d781edeee6a167d55354a1d213bbeb
enesarmagan7 <144248210+enesarmagan7@users.noreply.github.com>
2023-09-28 14:20:04 +0300

    Update TicketPriceCalculator.java

--- TicketPriceCalculator.java
+++ TicketPriceCalculator.java
@@ -3,10 +3,12 @@ package Letcode;
 import java.util.Scanner;
 
 public class TicketPriceCalculator {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        //Degiskenlerin tanimlanmasi
+        
         int age, km, flightchoice;
         double price = 0;
-
+//Kullanicidan parametrelerin alinmasi
         Scanner sc = new Scanner(System.in);
         System.out.println("Lütfen yolculuk mesafenizi km türünden giriniz.");
         km = sc.nextInt();
@@ -21,20 +23,21 @@ public class TicketPriceCalculator {
         } else {
             price = km * (0.10);
 
-            if (age < 12) {
+            if (age < 12) {                       //12 yaşından kucuk ise %50 indirim uygulanir.
                 price -= price*(0.50);
             } else if (age > 12 && age < 24) {
-
+                                                  //12 ile 24 yaş araliginda ise %10 indirim uygulanir.
                 price -= price*(0.10);
-            } else if (age > 65) {
+            } else if (age > 65) {                //65 yasindan büyükse %30 indirim uygulanir.
+                5                                              
                 price -= price*(0.30);
             }
 
-            if (flightchoice == 2) {
+            if (flightchoice == 2) {              // Gidis-Donus tipi secildiginde %20 indirim uygulanir.
                 price*=2;
                 price -= price*(0.20);
             }
-        }
+        }                                         
         System.out.println("Toplam Bilet Tutarı: "+price+" TL");
     }
 }
